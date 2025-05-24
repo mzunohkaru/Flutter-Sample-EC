d6e2af4a07c71d610abb6829532b177e1296f44a
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-05-24 12:09:28 +0900

    update: package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:flutter_stripe/flutter_stripe.dart' hide Card;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
